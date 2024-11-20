e01e436c81e76028da3008127898539b1011cb09
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-11-20 12:12:55 +0530

    feat variable scopes in javascript

---
+++ 101_javascript_typescript/01_basics/build/js/02_scope.js
@@ -0,0 +1,13 @@
+"use strict";
+function doSomething() {
+    var as1 = 12;
+}
+// console.log(as1);
+//Above shit does not work as the as1 is declared in the function block and it's access or visibility is within that block.
+let as2 = 12;
+{
+    //   console.log(as2); // here we get reference error because the block scope variable t is not initialized and it is used before ot's declaration
+    let as2 = 10;
+}
+//The const and let are block scope variables
+//const variable declaration must be initialized at point when they get declared
